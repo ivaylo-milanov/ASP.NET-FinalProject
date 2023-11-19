799a18d03e4e17d2165c4515cbc13e1becc7e1ec
ivaylo-milanov <imilanov24@gmail.com>
2023-11-20 00:53:08 +0200

    Added mediator folders in the core project

---
+++ HardwareStore.Core/ShoppingCart/ShoppingCart.cs
@@ -0,0 +1,12 @@
+﻿namespace HardwareStore.Core.ShoppingCart
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class ShoppingCart
+    {
+    }
+}
